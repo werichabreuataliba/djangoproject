c9d650be108eb8dd2af73d05eb488d257b27d185
wataliba <wataliba@brq.com>
2022-01-27 16:37:28 -0300

    save changes

--- heloworld/settings.py
+++ heloworld/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7=m9fg(rkh^e8e$_w9i&uo)v)bmuxvk!bhv4jkf*8s(%2x**a_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://heloworld-dj.herokuapp.com']
 
 
 # Application definition
